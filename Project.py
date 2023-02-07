0affe47b41e1149d8867d700bd71e65ab49b31c9
kumy123 <Kumy3797@gmail.com>
2023-02-07 14:53:09 +0800

    fr

--- Project.py
+++ Project.py
@@ -4,9 +4,4 @@ import numpy as np
 project = pd.read_excel('Project_File.xlsx')
 print(project)
 
-print('hello')
-<<<<<<< HEAD
-=======
-trgndhmhj
-testing123
->>>>>>> 6095fbe1d930f03062c2bf7a1c55d52c152dccb8
+
