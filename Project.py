3a527d3dc8f39d8e5d15a45a1146c56f15156b90
kumy123 <Kumy3797@gmail.com>
2023-02-14 11:15:50 +0800

    Update Project.py

--- Project.py
+++ Project.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 
-<<<<<<< HEAD
+
 project_data = pd.read_excel('Project_File.xlsx')
 print(project_data)
 project = pd.DataFrame(project_data)
@@ -9,11 +9,5 @@ print(project)
 
 Column = ["Saudi Arabia","Kuwait","UAE","United Kingdom","Germany","France","Italy","Netherlands","Greece","Belgium & Luxembourg","Switzerland","Austria","Scandinavia","CIS & Eastern Europe","USA","Canada","Australia","New Zealand","Africa"]
 project.drop(["Africa "],inplace=True,axis=1)
-=======
-missing_values = ["na"]
-project_df = pd.read_csv('Project_File.csv', na_values=missing_values)
-print(project_df)
-project_df.replace('na', np.NaN)
 
-print(project_df.isnull())
->>>>>>> b6f4a8da3b08674feffd15919d5981971ccc0b03
+
