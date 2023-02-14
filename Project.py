5e005218ffa6cc5901ebf3239336bbe60cd01566
Winger29 <115436141+Winger29@users.noreply.github.com>
2023-02-14 11:07:54 +0800

    Update Project.py

--- Project.py
+++ Project.py
@@ -2,8 +2,9 @@ import pandas as pd
 import numpy as np
 
 project = pd.read_excel('Project_File.xlsx')
+print(project_data)
+project = pd.DataFrame(project_data)
 print(project)
 
-
-
-
+Column = ["Saudi Arabia","Kuwait","UAE","United Kingdom","Germany","France","Italy","Netherlands","Greece","Belgium & Luxembourg","Switzerland","Austria","Scandinavia","CIS & Eastern Europe","USA","Canada","Australia","New Zealand","Africa"]
+project.drop(["Africa "],inplace=True,axis=1)
