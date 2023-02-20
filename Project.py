d63024c003ca31707893c8c85ff1c361e7f96ec8
Winger29 <115436141+Winger29@users.noreply.github.com>
2023-02-20 09:42:18 +0800

    Update Project.py

--- Project.py
+++ Project.py
@@ -5,6 +5,7 @@ import numpy as np
 project_data = pd.read_excel('Project_File.xlsx')
 print(project_data)
 project = pd.DataFrame(project_data)
+project.to_numpy()
 print(project)
 
 Column = ["Saudi Arabia","Kuwait","UAE","United Kingdom","Germany","France","Italy","Netherlands","Greece","Belgium & Luxembourg","Switzerland","Austria","Scandinavia","CIS & Eastern Europe","USA","Canada","Australia","New Zealand","Africa"]
