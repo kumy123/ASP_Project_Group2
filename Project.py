26348a36af07420ad6fb2b3d39f2a9e5ac0cbd3a
Winger29 <115436141+Winger29@users.noreply.github.com>
2023-02-22 14:42:12 +0800

    Update Project.py

--- Project.py
+++ Project.py
@@ -2,44 +2,55 @@ import pandas as pd
 import numpy as np
 
 
+
+#region = asia
+#era = 2008-2017
+
+
+#load data from excel
 project_data = pd.read_excel('Project_File.xlsx')
 print(project_data)
 project = pd.DataFrame(project_data)
-project.to_numpy()
+print(project)
+print(project.columns)
+
+
+#filter out by region selected
+project = project.drop(project.loc[:,' United Kingdom ':' Africa '].columns, axis=1)
 print(project)
 
-Column = ["Saudi Arabia","Kuwait","UAE","United Kingdom","Germany","France","Italy","Netherlands","Greece","Belgium & Luxembourg","Switzerland","Austria","Scandinavia","CIS & Eastern Europe","USA","Canada","Australia","New Zealand","Africa"]
-project = project.drop(project.loc[:,' Saudi Arabia ':' Africa '].columns, axis=1)
-print(project)
 
+
+print(project)
 
 print(project)
 print(project.columns)
 
 
-project.replace('na', 0)
-
 print(project.isnull().sum())
 
 print(project)
-
+#remove Na values from dataframe itself
 project = project.replace(' na ', np.nan)
-
 project = project.rename(columns={'   ':'Date'})
-
-
 project = project.dropna()
-
 print(project)
+
+
+
 project.info()
+#filter the dataframe by era
 project['Date'] = project['Date'].astype(str)
 print(type(project['Date']))
 print(project)
 project['Date'] = project['Date'].str.strip()
 project.info()
+print(project.columns)
 
 project['Date'] = pd.to_datetime(project['Date'], errors='coerce')
 print(project['Date'])
+print(project)
 #project['Date'] = project.loc[project['Date'].dt.year < 2007-12-01]
-project2 = project[(project['Date'] > "2007-12-01")]
-print(project2)
+
+
+#10 presentation(fuck that)
