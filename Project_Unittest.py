fc32fde48ef155184b6157e2d7c4d7f46c432a4e
Winger29 <115436141+Winger29@users.noreply.github.com>
2023-02-23 13:38:53 +0800

    Update Project_Unittest.py

---
+++ Project_Unittest.py
@@ -0,0 +1,14 @@
+import unittest
+import Project as proj
+
+class TestMyProgram(unittest.TestCase):
+
+    def test_EngineType(self):
+        print('testing 123')
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
+
