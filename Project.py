260b467a910f85ed775e38a3c7ec24715d08d122
Winger29 <115436141+Winger29@users.noreply.github.com>
2023-02-23 10:17:22 +0800

    Update Project.py

--- Project.py
+++ Project.py
@@ -12,7 +12,6 @@ project_data = pd.read_excel('Project_File.xlsx')
 print(project_data)
 project = pd.DataFrame(project_data)
 print(project)
-print(project.columns)
 
 
 #filter out by region selected
@@ -20,37 +19,74 @@ project = project.drop(project.loc[:,' United Kingdom ':' Africa '].columns, axi
 print(project)
 
 
-
-print(project)
-
-print(project)
-print(project.columns)
-
-
-print(project.isnull().sum())
-
-print(project)
-#remove Na values from dataframe itself
-project = project.replace(' na ', np.nan)
+#rename col to date
 project = project.rename(columns={'   ':'Date'})
-project = project.dropna()
-print(project)
 
-
-
-project.info()
-#filter the dataframe by era
+#removing whitespace for filtering year
 project['Date'] = project['Date'].astype(str)
-print(type(project['Date']))
-print(project)
 project['Date'] = project['Date'].str.strip()
-project.info()
-print(project.columns)
+project['Date'] = project['Date'].str.split(' ',2).str[0]
+project['Date'] = project['Date'].astype(int)
 
-project['Date'] = pd.to_datetime(project['Date'], errors='coerce')
-print(project['Date'])
+#filter df by years
+project = project.loc[(project['Date'] > 2007)]
+project = project.reset_index(drop=True)
 print(project)
-#project['Date'] = project.loc[project['Date'].dt.year < 2007-12-01]
+
+#Q6
+asia = [' Brunei Darussalam ', ' Indonesia ', ' Malaysia ',
+       ' Philippines ', ' Thailand ', ' Viet Nam ', ' Myanmar ', ' Japan ',
+       ' Hong Kong ', ' China ', ' Taiwan ', ' Korea, Republic Of ', ' India ',
+       ' Pakistan ', ' Sri Lanka ', ' Saudi Arabia ', ' Kuwait ', ' UAE ']
+sum_col= project[asia].sum(axis=0)
+print(sum_col)
+sum_dict = sum_col.to_dict()
+print(sum_dict)
+#sum_col= pd.DataFrame(project[asia].sum(axis=0), columns=['total']).reset_index()
+#sum_col = sum_col.rename(columns={'index':'Countries'})
+countries = list(sum_dict.keys())
+total = list(sum_dict.values())
+import matplotlib.pyplot as plt
+plt.figure(facecolor='white',figsize=(10,10))
+plt.bar(x = countries, height = total, color = 'orange' )
+plt.ticklabel_format(axis='y',style='plain')
+plt.xticks(rotation = 45)
+plt.xlabel('Countries')
+plt.ylabel('total no. of visitors')
+plt.show()
 
 
-#10 presentation(fuck that)
+#Q7
+sorted_col = sum_col.sort_values(ascending=False)
+print(sorted_col)
+top3_val = sorted_col.head(3)
+print(top3_val)
+
+
+#sorted_value =
+#top3_region
+#sort value and then sort it by top 3 values of countries in the region selected
+
+
+#Q8
+#do the unittest, make sure to import unittest
+import unittest
+
+class TestMyProgram(unittest.TestCase):
+
+       def __int__(self):
+              self.file = file
+
+
+       def test_load_data(self):
+              load_data = pd.read_excel(self.file)
+              return load_data
+
+
+if __name__ == '__main__':
+       unittest.main()
+
+
+
+
+#09 presentation(fuck that)
